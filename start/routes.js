52cdc2bd8824c25415ccb59ac9f04ffd8b73686b
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-05-11 18:59:49 +0000

    Fix typo on register page route name

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/register').render('auth.register').as('regiter')
+Route.on('/register').render('auth.register').as('register')
 Route.post('/register', 'AuthController.register')
 
 Route.on('/').render('home').as('home')
