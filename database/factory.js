8bbcbe42b2c4d5ac0df190dac0c06687d2254c22
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-05-12 11:25:58 +0000

    Create user factory

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,14 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use('Hash')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    username: faker.username(),
+    email: faker.email(),
+    password: await Hash.make(faker.password())
+  }
+})
