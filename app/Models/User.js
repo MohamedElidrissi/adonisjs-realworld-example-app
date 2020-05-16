485ae172e3a2a7b7a771f53b10b66b40cde8671d
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-05-16 13:44:18 +0000

    Create Article model

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,10 @@ class User extends Model {
     })
   }
 
+  articles() {
+    return this.hasMany('App/Models/Article')
+  }
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
---
+++ database/migrations/1589635211877_article_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ArticleSchema extends Schema {
+  up() {
+    this.create('articles', (table) => {
+      table.increments()
+      table.string('title').notNullable()
+      table.string('desc').notNullable()
+      table.text('body').notNullable()
+      table.string('tags').nullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .notNullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('articles')
+  }
+}
+
+module.exports = ArticleSchema
